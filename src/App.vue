bb9af7ae09cf821ef02def4f0aff61e360b8ee5d
FloraHmmnd <flora.hommand@jellysmack.com>
2022-12-23 12:31:25 +0100

    ✨ hello world

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+    <span>HelloWorld</span>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
