fb9ce23f348b33eb67d6b0f3f93fc5855adc635d
FloraHmmnd <flora.hommand@jellysmack.com>
2022-12-30 17:46:35 +0100

    ✨ adjust design

--- src/pages/Home.page.vue
+++ src/pages/Home.page.vue
@@ -1,14 +1,20 @@
 <template>
-    <h1>Note Libre</h1>
-        <button>
-            <router-link to="/play">Play now</router-link>
-        </button>
+    <h1 @mouseover="title='Play now'" 
+    @mouseleave="title='Note Libre'">
+        <router-link to="/play">{{ title }}</router-link>
+    </h1>
 </template>
+<script setup>
 
+import { ref } from "vue"
+
+const  title = ref("Note Libre")
+
+</script>
 <style scoped>
 
 h1{
-    text-shadow: 1px 1px 2px rgb(0, 255, 145), 0 0 1em rgb(6, 6, 6), 0 0 0.2em rgb(85, 103, 98);
+    text-shadow: 1px 1px 2px rgb(0, 221, 255), 0 0 1em rgb(6, 6, 6), 0 0 0.2em rgb(85, 103, 98);
 }
 
 </style>
