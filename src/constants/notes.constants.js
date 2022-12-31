0cc858540ef1e1a978e425c91944d2f976f01ee8
FloraHmmnd <flora.hommand@jellysmack.com>
2022-12-31 11:43:17 +0100

    ✨ adjust const NOTES

--- src/constants/notes.constants.js
+++ src/constants/notes.constants.js
@@ -2,72 +2,109 @@ export const NOTES = [
   {
     id: 1,
     name: "do",
-    types: [
-      { type: "", color: "#000000" },
-      { type: "#", color: "#ADACAC" },
-    ],
+    type: null,
+    color: "#000000",
   },
-
   {
     id: 2,
-    name: "ré",
-    types: [
-      { type: "", color: "#FF0000" },
-      { type: "#", color: "#FF8686" },
-      { type: "b", color: "#8D4646" },
-    ],
+    name: "do",
+    type: "#",
+    color: "#ADACAC",
   },
-
   {
     id: 3,
-    name: "mi",
-    types: [
-      { type: "", color: "#FB00FF" },
-      { type: "b", color: "#854486" },
-    ],
+    name: "ré",
+    type: null,
+    color: "#FF0000",
   },
-
   {
     id: 4,
-    name: "fa",
-    types: [
-      { type: "", color: "#0004FF" },
-      { type: "#", color: "#888AFE" },
-    ],
+    name: "ré",
+    type: "#",
+    color: "#FF8686",
   },
-
-  {
-    id: 4,
-    name: "sol",
-    types: [
-      { type: "", color: "#00FFF3" },
-      { type: "#", color: "#82FFF9" },
-      { type: "b", color: "#4A9693" },
-    ],
-  },
-
   {
     id: 5,
-    name: "la",
-    types: [
-      { type: "", color: "#00FF0C" },
-      { type: "#", color: "#83FF89" },
-      { type: "b", color: "#47894A" },
-    ],
+    name: "ré",
+    type: "b",
+    color: "#8D4646",
   },
-
   {
     id: 6,
-    name: "si",
-    types: [
-      { type: "", color: "#FFFB00" },
-      { type: "b", color: "#A1A04F" },
-    ],
+    name: "mi",
+    type: null,
+    color: "#FB00FF",
   },
-
   {
     id: 7,
+    name: "mi",
+    type: "b",
+    color: "#854486",
+  },
+  {
+    id: 8,
+    name: "fa",
+    type: null,
+    color: "#0004FF",
+  },
+  {
+    id: 9,
+    name: "fa",
+    type: "#",
+    color: "#888AFE",
+  },
+  {
+    id: 10,
+    name: "sol",
+    type: null,
+    color: "#00FFF3",
+  },
+  {
+    id: 11,
+    name: "sol",
+    type: "#",
+    color: "#82FFF9",
+  },
+  {
+    id: 12,
+    name: "sol",
+    type: "b",
+    color: "#4A9693",
+  },
+  {
+    id: 13,
+    name: "la",
+    type: null,
+    color: "#00FF0C",
+  },
+  {
+    id: 14,
+    name: "la",
+    type: "#",
+    color: "#83FF89",
+  },
+  {
+    id: 15,
+    name: "la",
+    type: "b",
+    color: "#47894A",
+  },
+  {
+    id: 16,
+    name: "si",
+    type: null,
+    color: "#FFFB00",
+  },
+  {
+    id: 17,
+    name: "si",
+    type: "b",
+    color: "#A1A04F",
+  },
+  {
+    id: 19,
     name: "soupir",
-    types: [{ type: "", color: "#FFFFFF" }],
+    type: null,
+    color: "#FFFFFF",
   },
 ];
