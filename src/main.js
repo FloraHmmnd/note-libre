b5464b5ba7e4b5d1ceee5961302c8984b59f4826
FloraHmmnd <flora.hommand@jellysmack.com>
2023-01-14 16:36:04 +0100

    ✨ add pinia and score store

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from "vue";
 import router from "./router";
+import { createPinia } from "pinia";
 
 import "./style.css";
 import App from "./App.vue";
 
-createApp(App).use(router).mount("#app");
+const pinia = createPinia();
+
+createApp(App).use(router).use(pinia).mount("#app");
