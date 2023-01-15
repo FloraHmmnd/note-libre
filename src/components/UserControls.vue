6d351e7a8a39e7785c8113b028d2c177ac69d9a8
FloraHmmnd <flora.hommand@jellysmack.com>
2023-01-15 14:49:31 +0100

    ✨ refacto store

--- src/components/UserControls.vue
+++ src/components/UserControls.vue
@@ -10,27 +10,6 @@
     />
   </div>
   <div>
-    <label for="min octave">Select minimum octave</label>
-    <input
-      type="number"
-      id="minOctave"
-      min="1"
-      max="8"
-      v-model="minOctaveSelected"
-    />
-  </div>
-  <div>
-    <label for="max octave">Select maximum octave</label>
-    <input
-      type="number"
-      id="maxOctave"
-      :min="minOctaveSelected"
-      max="8"
-      v-model="maxOctaveSelected"
-    />
-  </div>
-
-  <div>
     <label for="choiceTempo">Select a tempo</label>
     <select id="choiceTempo" v-model="selectedTempo">
       <option v-for="tempo in TEMPOS" :key="tempo.id" :value="tempo.name">
@@ -38,34 +17,41 @@
       </option>
     </select>
   </div>
+  <div class="select-cotaves">
+    <label>Select octaves min and max</label>
+    <VueSlider
+      v-model="octaveValue"
+      :min="1"
+      :max="8"
+      :interval="1"
+      @drag-end="setOctavesValues()"
+    ></VueSlider>
+  </div>
   <button @click="handleAction()">GO</button>
 </template>
 
 <script setup>
-import { watch } from "vue";
+import { ref } from "vue";
 import { storeToRefs } from "pinia";
 
 import useScoreStore from "../store/score.store";
 import { TEMPOS } from "../constants/tempos.constants";
 import useScoreGenerator from "../composables/useScoreGenerator.composables";
 
-const {
-  numberOfNotesSelected,
-  minOctaveSelected,
-  maxOctaveSelected,
-  selectedTempo,
-} = storeToRefs(useScoreStore());
+import VueSlider from "vue-slider-component";
+import "vue-slider-component/theme/default.css";
 
+const { numberOfNotesSelected, selectedTempo } = storeToRefs(useScoreStore());
+
+const { setMinOctave, setMaxOctave } = useScoreStore();
+const octaveValue = ref([1, 8]);
+
+const setOctavesValues = () => {
+  setMinOctave(octaveValue.value[0]);
+  setMaxOctave(octaveValue.value[1]);
+};
 const { player } = useScoreGenerator();
 const handleAction = () => {
   player();
 };
-watch(
-  () => minOctaveSelected.value,
-  (newValue) => {
-    if (newValue >= maxOctaveSelected.value) {
-      maxOctaveSelected.value = minOctaveSelected.value;
-    }
-  }
-);
 </script>
